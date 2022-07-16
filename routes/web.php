7144f411e166f3702cd129055666edc8a52d2f03
olashina201 <olashina201@gmail.com>
2022-07-16 08:01:52 +0100

    interface

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('jobs', [
+        'heading' => 'Latest Jobs',
+        'jobs' => Job::all()
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    return view('job', [
+        'jobs' => Job::find($id)
+    ]);
 });
