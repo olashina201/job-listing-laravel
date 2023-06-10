ed37d75e2f0888ba09a5c40ff7ae1f3a7f53d764
olashina201 <olashina201@gmail.com>
2023-06-10 05:29:36 +0100

    jnkw

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,17 @@ Route::get('/jobs/{id}/edit', [JobController::class, 'edit']);
 Route::put('/jobs/{id}', [JobController::class, 'update']);
 Route::delete('/jobs/{id}', [JobController::class, 'destroy']);
 Route::get('/jobs/{id}', [JobController::class, 'show']);
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create'])->middleware('guest');
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
+
+// Log User Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show Login Form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Log In User
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
